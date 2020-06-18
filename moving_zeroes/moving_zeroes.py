cb93d181847a4b6047688e1fb76b6e2111f65582
Mario Rivera <mario@Marios-MacBook-Pro.local>
2020-06-17 23:14:21 -0700

    finished moving zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -5,6 +5,13 @@ Returns: a List of integers
 def moving_zeroes(arr):
     # Your code here
     #iterate through list
+    for num in arr:
+        if num == 0:
+            # print(num)
+            arr.append(num)
+            del arr[arr.index(0)]
+    return arr[0:]
+        
     #compare element to zero
     #zeros have indez changed to list length(-1)
 
